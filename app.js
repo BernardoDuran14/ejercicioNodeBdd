a52d08ebb6c8713320ba67875102cebc63f5ebf3
BernardoDuran14 <bernardo.duran.mamani@gmail.com>
2025-05-01 21:43:47 -0400

    Configuracion del app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require('express');
+const express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -6,7 +6,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+const clienteRoutes = require('./routes/cliente.routes');
+const pedidoRoutes = require('./routes/pedido.routes');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +19,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', clienteRoutes);
+app.use('/api', pedidoRoutes);
+const PORT = process.env.PORT || 3100;
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
 
 module.exports = app;
